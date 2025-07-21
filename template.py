e852aff34956e4fcafb36dcd28f00e0a3d3ca430
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-21 23:50:44 +0500

    added the files that needed

--- template.py
+++ template.py
@@ -9,15 +9,11 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    "backend/ingest_data.py",
-    "backend/model.py",
-    "static/style.css",
+    "backend/generate_blog.py",
     "app.py",
     ".env",
     "Dockerfile",
     "requirements.txt",
-    "templates/index.html",
-    "test.py"
 ]
 
 
@@ -37,4 +33,10 @@ for filepath in list_of_files:
 
     if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging .info(f"{filename} is already exists")
